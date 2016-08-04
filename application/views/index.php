9970de52893bafe736567109bd0ef81fda1c5c07
manurockz007 <manurockz007@gmail.com>
2016-08-04 15:32:20 +0530

    Add files via upload

---
+++ application/views/index.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Manohar
+ * Date: 8/4/2016
+ * Time: 12:16 PM
+ */
+?>
+<section id="main-slider" class="no-margin">
+    <div class="carousel slide">
+        <ol class="carousel-indicators">
+            <li data-target="#main-slider" data-slide-to="0" class="active"></li>
+            <li data-target="#main-slider" data-slide-to="1"></li>
+            <li data-target="#main-slider" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+
+            <div class="item active" style="background-image: url(<?php echo base_url(); ?>images/slider/bg1.jpg)">
+                <div class="container">
+                    <div class="row slide-margin">
+                        <div class="col-sm-6">
+                            <div class="carousel-content">
+                                <h1 class="animation animated-item-1">Has life been like a combat zone, troubles shooting at you from every side. Are you Angry? Depressed? Unhappy? Unloved?</h1>
+                                <h2 class="animation animated-item-2">You've heard and come to believe that if you smile, the world smiles with you, cry and you cry alone. And so, you've been holding it all in, calm face to the wind, turmoil within.
+                                    I am your virtual listener.I don't talk,I listen.</h2>
+                                <a class="btn-slide animation animated-item-3" href="#">Read More</a>
+                            </div>
+                        </div>
+
+                        <div class="col-sm-6 hidden-xs animation animated-item-4">
+                            <div class="slider-img">
+                                <img src="<?php echo base_url(); ?>images/slider/img1.png" class="img-responsive">
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div><!--/.item-->
+
+            <div class="item" style="background-image: url(<?php echo base_url(); ?>images/slider/bg2.jpg)">
+                <div class="container">
+                    <div class="row slide-margin">
+                        <div class="col-sm-6">
+                            <div class="carousel-content">
+                                <h1 class="animation animated-item-1">Virtual - what?
+                                    Is there a catch somewhere? No, there isn't.</h1>
+                                <h2 class="animation animated-item-2">I am here to listen to you because I've seen lonely lives fall apart.
+                                    I am here to listen because I know what it is like to never be heard.
+                                    One day, we could meet in person, if you need more counseling.
+                                    But for right now, I am your virtual friend.</h2>
+                                <a class="btn-slide animation animated-item-3" href="#">Read More</a>
+                            </div>
+                        </div>
+
+                        <div class="col-sm-6 hidden-xs animation animated-item-4">
+                            <div class="slider-img">
+                                <img src="<?php echo base_url(); ?>images/slider/img2.png" class="img-responsive">
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div><!--/.item-->
+            <div class="item" style="background-image: url(<?php echo base_url(); ?>images/slider/bg3.jpg)">
+                <div class="container">
+                    <div class="row slide-margin">
+                        <div class="col-sm-6">
+                            <div class="carousel-content">
+                                <h1 class="animation animated-item-1">Lorem ipsum dolor sit amet consectetur adipisicing elit</h1>
+                                <h2 class="animation animated-item-2">Accusantium doloremque laudantium totam rem aperiam, eaque ipsa...</h2>
+                                <a class="btn-slide animation animated-item-3" href="#">Read More</a>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 hidden-xs animation animated-item-4">
+                            <div class="slider-img">
+                                <img src="<?php echo base_url(); ?>images/slider/img3.png" class="img-responsive">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div><!--/.item-->
